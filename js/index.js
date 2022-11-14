4c55c540ed78a85202da5f6fa01b0950b061ac38
Arsenic99 <arsen09kz@gmail.com>
2022-11-15 01:39:29 +0600

    Bug with itemsInCard

--- js/index.js
+++ js/index.js
@@ -3,25 +3,37 @@
 itemsInCard();
 function itemsInCard(){
 let itemsInCard = JSON.parse(localStorage.getItem("card"));
-const item__number = document.querySelector('.item__number');
-const card__status = document.querySelector('.card__status');
+const item__number = Array.from(document.querySelectorAll('.item__number'));
+const card__status = Array.from(document.querySelectorAll('.card__status'));
+console.log(item__number);
+console.log(card__status);
 if (itemsInCard.length > 0) {
-    item__number.innerHTML = itemsInCard.length;
-    card__status.style.display = "inline-block";
+    item__number.map((item)=>{
+        item.innerHTML = itemsInCard.length;
+    })
+    
+    card__status.map((item)=>{
+        item.style.display = "inline-block";
+    })
 }
 else
 {
-    card__status.style.display = "none";
+    card__status.map((item)=>{
+        item.style.display = "none";
+    })
 }
 }
-
 //Ссылка на аккаунт
 
-const navigation__account = document.querySelector('.navigation__account');
+const navigation__account = Array.from(document.querySelectorAll('.navigation__account'));
 
-navigation__account.addEventListener(("click"),()=>{
-    window.location.href = "http://127.0.0.1:5500/pages/auth.html";
+navigation__account.map((item)=>{
+    item.addEventListener(("click"),()=>{
+        console.log(1);
+        window.location.href = "http://127.0.0.1:5500/pages/auth.html";
+    })
 })
+
 
 //Анимация бургер меню
 const burger__menu_btn = document.querySelector('.burger__menu-button');
